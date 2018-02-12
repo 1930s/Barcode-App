47032b1e3ccd4e6013735ffe5966382e82a890b2
simdimlim <oldskooluv@hotmail.com>
2018-02-12 14:09:08 +1100

    imported qr code reader starter code

---
+++ QRCodeReader/QRCodeReader/QRCodeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  QRCodeViewController.swift
+//  QRCodeReader
+//
+//  Created by Simon Ng on 13/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+var captureSession:AVCaptureSession?
+var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+var qrCodeFrameView:UIView?
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Get the back-facing camera for capturing videos
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera device")
+            return
+        }
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Set the input device on the capture session.
+            captureSession.addInput(input)
+        } catch {
+            // If any errors occurs, simply print it out and don't continue anymore.
+            print(error)
+            return
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+}
